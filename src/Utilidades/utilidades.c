d75276921030b8b0144d2ea15308a096803b3c38
LanderLorenzo <33033661+LanderLorenzo@users.noreply.github.com>
2018-04-03 18:51:41 +0200

    Creado el src, carpeta utilidades,estructura de .c y.h

---
+++ src/Utilidades/utilidades.c
@@ -0,0 +1,8 @@
+/*
+ * utilidades.c
+ *
+ *  Created on: 3 abr. 2018
+ *      Author: Lander
+ */
+
+#include "utilidades.h"
---
+++ src/Utilidades/utilidades.h
@@ -0,0 +1,13 @@
+/*
+ * utilidades.h
+ *
+ *  Created on: 3 abr. 2018
+ *      Author: Lander
+ */
+
+#ifndef UTILIDADES_H_
+#define UTILIDADES_H_
+
+
+
+#endif /* UTILIDADES_H_ */

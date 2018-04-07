d0720c48e8d789b3536f30a573c2046d6bd76c5b
LanderLorenzo <33033661+LanderLorenzo@users.noreply.github.com>
2018-04-07 17:17:12 +0200

    Comencemos con los menus.

---
+++ src/menus.c
@@ -0,0 +1,8 @@
+/*
+ * menus.c
+ *
+ *  Created on: 7 abr. 2018
+ *      Author: Lander
+ */
+
+
---
+++ src/menus.h
@@ -0,0 +1,13 @@
+/*
+ * menus.h
+ *
+ *  Created on: 7 abr. 2018
+ *      Author: Lander
+ */
+
+#ifndef MENUS_H_
+#define MENUS_H_
+
+
+
+#endif /* MENUS_H_ */

daf8d424804f90708d162b207b2b1d8f9435e20c
DiezAndino.Daniel <33398625+Drakensw@users.noreply.github.com>
2018-04-07 18:27:28 +0200

    Menu Jugar

--- src/menus.c
+++ src/menus.c
@@ -23,6 +23,20 @@ void MenuPrincipal(){
 }
 
 void MenuJugar(){
+	int eleccion;
+
+	printf("------------------------------------------------------------------- \n");
+	printf("JUGAR \n");
+	printf("1. Historia 1 \n");
+	printf("2. Historia 2 \n");
+	printf("3. Historia 3 \n");
+	printf("4. Historia 4 \n");
+	printf("5. Atras \n");
+	printf("¿Que opción desea?: ");
+	fflush(stdin);
+	fflush(stdout);
+	scanf("%d" , &eleccion);
+	printf("\n Tu elección es: %d \n" , eleccion);
 
 }
 

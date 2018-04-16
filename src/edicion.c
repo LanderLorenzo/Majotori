211e4900e0b9a7598f6be93e613ee4479a47af17
LanderLorenzo <33033661+LanderLorenzo@users.noreply.github.com>
2018-04-16 10:30:17 +0200

    creacion de edicion.c y .h

---
+++ src/edicion.c
@@ -0,0 +1,8 @@
+/*
+ * edicion.c
+ *
+ *  Created on: 16 abr. 2018
+ *      Author: Lander
+ */
+#include "edicion.h"
+
---
+++ src/edicion.h
@@ -0,0 +1,13 @@
+/*
+ * edicion.h
+ *
+ *  Created on: 16 abr. 2018
+ *      Author: Lander
+ */
+
+#ifndef EDICION_H_
+#define EDICION_H_
+
+
+
+#endif /* EDICION_H_ */
